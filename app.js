ebb84f4a28c88f5193652e838425c152a86f614f
Jarrod Savard <Jarrodsavard2x180@gmail.com>
2023-05-15 16:11:42 -0700

    changed package.json type to module && added openai dep && tested new generate key

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log('hello world')
+import 'dotenv/config';
+
+import { Configuration, OpenAIApi } from 'openai';
+const configuration = new Configuration({
+	apiKey: process.env.OPENAI_KEY,
+});
+const openai = new OpenAIApi(configuration);
+const response = await openai.createCompletion({
+	model: 'text-davinci-003',
+	prompt: 'Say this is a test',
+	temperature: 0,
+	max_tokens: 7,
+});
+
+console.log(response.data.choices[0].text);
